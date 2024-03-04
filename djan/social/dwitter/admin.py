12e9d72676106df78d6b0a5aac1e3d0892b32e27
shubigsh <136429877+shubigsh@users.noreply.github.com>
2024-03-04 18:55:43 +0530

    Add files via upload

---
+++ djan/social/dwitter/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.contrib.auth.models import User, Group
+from .models import Dweet, Profile
+
+# ...
+admin.site.register(Profile)
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ["username"]
+    inlines = [ProfileInline]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
+admin.site.register(Dweet)
+
+# Remove: admin.site.register(Profile)
+
+
